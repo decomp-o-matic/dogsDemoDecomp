47444c6debb81a72f7366d65c9c1096bcb4f114b
Urocu <103362793+Urocu@users.noreply.github.com>
2023-12-04 22:39:24 +0100

    added create_Window function (WIP)

--- demonstrator.cpp
+++ demonstrator.cpp
@@ -1,5 +1,53 @@
 char* g_IllegalWarning = {};
 
+//004117c0
+create_Window(void *this,undefined4 CmdLine,int ShowCmd,LPCSTR WindowName,HICON Icon,HICON IconSm,int width,int height)
+{
+	ATOM atomWin;
+	int iVar1;
+	uint uVar2;
+	int *extraout_ECX;
+	int *piVar3;
+	CHAR local_4b0 [1024];
+	undefined4 local_b0;
+	int **local_ac;
+	undefined4 local_a8 [16];
+	tagMSG local_68;
+	undefined *local_4c;
+	WNDCLASSEXA winClass;
+	int *local_18;
+	int *local_14;
+	void *vExceptionList;
+	undefined *puStack_c;
+	undefined4 local_8;
+	
+	//...
+	
+	winClass.cbSize = 0x30;
+	winClass.style = 3;
+	winClass.lpfnWndProc = (WNDPROC)&LAB_00411590;
+	winClass.cbClsExtra = 0;
+	winClass.cbWndExtra = 0;
+	winClass.hInstance = mainHInstance;
+	winClass.hIcon = Icon;
+	winClass.hCursor = LoadCursorA((HINSTANCE)0x0,(LPCSTR)0x7f00);
+	winClass.hbrBackground = (HBRUSH)0x0;
+	winClass.lpszMenuName = (LPCSTR)0x0;
+	winClass.lpszClassName = (LPCSTR)&lpClassName_004667b4;
+	winClass.hIconSm = IconSm;
+	atomWin = RegisterClassExA(&winClass);
+	if (atomWin != 0) {
+		mainHWND = CreateWindowExA(0,(LPCSTR)&lpClassName_004667b4,WindowName,0xc00000,-0x80000000,-0x80000000,width,height,(HWND)0x0,(HMENU)0x0,mainHInstance,(LPVOID)0x0);
+		if (mainHWND != (HWND)0x0) {
+			ShowWindow(mainHWND,ShowCmd);
+			UpdateWindow(mainHWND);
+			//...
+		}
+		DestroyWindow(mainHWND);
+	}
+	
+}
+
 //004016f0
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
--- demonstrator.h
+++ demonstrator.h
@@ -1,1 +1,6 @@
 #include <windows.h>
+
+//004117c0
+unsigned int create_Window(void *this,undefined4 CmdLine,int ShowCmd,LPCSTR WindowName,HICON Icon,HICON IconSm,int width,int height);
+//004016f0
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd);
